597649a534a9365d24ba568d1acb7214fece6f8d
anjezzaa <anjeza.murati@gmail.com>
2026-01-13 16:51:23 +0100

    multi-element components

--- src/app/shared/button/button.component.ts
+++ src/app/shared/button/button.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'button[app-button]',
+  selector: 'button[app-button], a[apputton]',
   standalone: true,
   imports: [],
   templateUrl: './button.component.html',
