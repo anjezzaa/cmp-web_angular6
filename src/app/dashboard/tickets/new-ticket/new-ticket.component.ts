c8fcf4e5e8dca70850b4eda621133e0045fd4780
anjezzaa <anjeza.murati@gmail.com>
2026-01-19 12:34:13 +0100

    component outputs

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, OnInit, viewChild, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, EventEmitter, OnInit, output, Output, viewChild, ViewChild } from '@angular/core';
 import { ButtonComponent } from "../../../shared/button/button.component";
 import { ControlComponent } from "../../../shared/control/control.component";
 import { FormsModule } from '@angular/forms'
@@ -12,6 +12,9 @@ import { FormsModule } from '@angular/forms'
 export class NewTicketComponent implements OnInit, AfterViewInit {
     @ViewChild('form') form?: ElementRef<HTMLFormElement>;
     // private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
+    // @Output() add = new EventEmitter()
+
+    add = output<{title:string; text:string}>();
 
     ngOnInit() {
       console.log('ONINIT');
@@ -24,8 +27,9 @@ export class NewTicketComponent implements OnInit, AfterViewInit {
     }
 
     onSubmit(title:string, ticketText:string) {
-      console.log(title);
-      console.log(ticketText);
+      this.add.emit({title: title, text:ticketText});
+      // console.log(title);
+      // console.log(ticketText);
       this.form?.nativeElement.reset();
     }
 
