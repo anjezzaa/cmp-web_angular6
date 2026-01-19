ead08a76eb37721ec930efe893bc9e8286f04771
anjezzaa <anjeza.murati@gmail.com>
2026-01-19 15:15:58 +0100

    inputs&signals

--- src/app/dashboard/tickets/ticket/ticket.component.ts
+++ src/app/dashboard/tickets/ticket/ticket.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { NgIf } from "../../../../../node_modules/@angular/common/index";
+import { Ticket } from './ticket.model';
 
 @Component({
   selector: 'app-ticket',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './ticket.component.html',
   styleUrl: './ticket.component.css'
 })
 export class TicketComponent {
-
+  // @Input 
+  data = input.required<Ticket>();
 }
